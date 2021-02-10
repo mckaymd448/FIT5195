0519c4726602e99ed0fead796d58841bc866b71f
michaelmckay <michaelmckay@vpn-118-138-189-78.its.monash.edu.au>
2021-02-10 12:47:56 +0800

    Done up to report 7.

--- Assignment 1/Ass_01b_task3.sql
+++ Assignment 1/Ass_01b_task3.sql
@@ -1,6 +1,6 @@
 --/ Simple reports:
 
---/ Query to get a ranked list of which months had the most new members, and which postcode they were located in.
+--/ Report 1.  Query to get a ranked list of which months had the most new members, and which postcode they were located in.
 SELECT
     *
 FROM
@@ -27,7 +27,7 @@ FROM
 WHERE
     month_rank <= 3;
     
---/ Query to get percentage of workout goal grouped by date.
+--/ Report 2.  Query to get percentage of workout goal grouped by date.
 
 SELECT
     dd.year,
@@ -49,7 +49,7 @@ ORDER BY
     dd.year,
     gd.training_goal;
     
---/ Training salary and date query.
+--/ Report 2.  Training salary and date query.
 
 SELECT
     dd.year,
@@ -73,12 +73,12 @@ ORDER BY
     sd.sal_range_id;
     
     
---/ 
+--/ Report 3.  
 SELECT
-    dd.month,
-    cd.suburb_id,
-    md.member_type,
-    SUM(cf.membership_fee_total) AS "total_membership_fee"
+    decode(GROUPING(dd.month), 1, 'All Months', dd.month)                                AS month,
+    decode(GROUPING(cd.suburb_id), 1, 'All Suburbs', cd.suburb_id)                       AS suburb,
+    decode(GROUPING(md.member_type), 1, 'All Memberships', md.member_type)               AS member_type,
+    SUM(cf.membership_fee_total)                                                         AS "total_membership_fee"
 FROM
          clientfact cf
     JOIN memberdim     md ON cf.member_id = md.member_id
@@ -90,3 +90,85 @@ GROUP BY
          md.member_type)
 ORDER BY
     dd.month;
+    
+--/ Report 4.  
+SELECT
+    dd.month,
+    decode(GROUPING(cd.suburb_id), 1, 'All Suburbs', cd.suburb_id)                       AS suburb,
+    decode(GROUPING(md.member_type), 1, 'All Memberships', md.member_type)               AS member_type,
+    SUM(cf.membership_fee_total)                                                         AS "total_membership_fee"
+FROM
+         clientfact cf
+    JOIN memberdim     md ON cf.member_id = md.member_id
+    JOIN clientsubdim  cd ON cf.suburb_id = cd.suburb_id
+    JOIN datedim       dd ON dd.date_id BETWEEN md.member_start_year || md.member_start_month AND md.member_end_year || md.member_end_month
+GROUP BY
+    dd.month,
+    CUBE(cd.suburb_id,
+         md.member_type)
+ORDER BY
+    dd.month;
+    
+--/ Report 5.
+
+SELECT
+    decode(GROUPING(dd.year), 1, 'All years', dd.year)                                 AS year,
+    decode(GROUPING(dd.month), 1, 'All months', dd.month)                              AS month,
+    decode(GROUPING(gd.training_goal), 1, 'All goals', gd.training_goal)               AS goal,
+    SUM(wf.workout_total)                                                              AS total_workouts
+FROM
+         workoutfact wf
+    JOIN goaldim  gd ON wf.training_goal = gd.training_goal
+    JOIN datedim  dd ON wf.work_date_id = dd.date_id
+GROUP BY
+    ROLLUP(dd.year,
+           dd.month,
+           gd.training_goal)
+ORDER BY
+    dd.year,
+    dd.month;
+    
+--/ Report 6.
+
+SELECT
+    dd.year,
+    dd.month,
+    decode(GROUPING(gd.training_goal), 1, 'All goals', gd.training_goal)               AS goal,
+    SUM(wf.workout_total)                                                              AS total_workouts
+FROM
+         workoutfact wf
+    JOIN goaldim  gd ON wf.training_goal = gd.training_goal
+    JOIN datedim  dd ON wf.work_date_id = dd.date_id
+GROUP BY
+    dd.year,
+    dd.month,
+    ROLLUP(gd.training_goal)
+ORDER BY
+    dd.year,
+    dd.month;
+    
+--/ Report 7.
+
+SELECT
+    dd.year,
+    dd.month,
+    gd.training_goal,
+    SUM(wf.workout_total) AS total_workouts
+    Sum(Sum(wf.workout total) over (partion BY dd.year
+    ORDER BY
+        dd.year,
+        dd.month
+    ROWS UNBOUNDED PRECEDING
+)                                AS cumulative_total
+FROM
+         workoutfact wf
+    JOIN goaldim  gd ON wf.training_goal = gd.training_goal
+    JOIN datedim  dd ON wf.work_date_id = dd.date_id
+WHERE
+    gd.training_goal = 'Injury Rehabilitation'
+GROUP BY
+    dd.year,
+    dd.month,
+    gd.training_goal
+ORDER BY dd.year,
+         dd.month;
